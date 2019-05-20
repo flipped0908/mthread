8dd3d9b23cb3f0401b8c12fd2cbf105d613c3452
gsh <kevin@yusi.studio>
2019-05-20 11:08:06 +0800

    1

---
+++ src/main/java/code02.java
@@ -0,0 +1,16 @@
+import com.sun.xml.internal.ws.util.CompletedFuture;
+
+import java.util.concurrent.CompletionStage;
+
+public class code02 <T>{
+
+
+    CompletionStage<T> completionStage ;
+
+    CompletedFuture<T> completedFuture;
+
+
+
+
+
+}
